e9a18bd09543fccc9c43c671edb9013a7537ee22
William Lundgren <superbanan@live.se>
2022-06-07 01:09:44 +0200

    Changed files to include ability 'number'

--- scrape_lol_abilities.py
+++ scrape_lol_abilities.py
@@ -26,18 +26,60 @@ def find_abilities(url):
 
     skills = soup.select("div.skill")
 
-    links = []
+    exclude = ["Aphelios", "Elise", "Gnar", "Jayce", "Jhin", "Kalista", "Kled",
+               "Nidalee", "Rek%27Sai", "Renata_Glasc", "Senna", "Zeri"]
+    if champ_name not in exclude:
+        prnt = ""
+        for i in skills:
+            try:
+                prnt += i.get("class")[1] + " "
+            except IndexError:
+                print(champ_name, i.get("class"))
+
+        if prnt != "skill_innate skill_q skill_w skill_e skill_r ":
+            print(champ_name, prnt)
+
+    links = {}
     used_abilities = []
 
-    for skill in skills:
-        imgs = skill.find_all("img")
-        for i in imgs:
-            if i.get("data-src") is not None and champ_name in i.get("data-src") and i.get("alt").split(".")[0] not in used_abilities and "scale" not in i.get("data-src"):
-                links.append(i.get("data-src"))
-                used_abilities.append(i.get("alt").split(".")[0])
+    # TODO
+    # Deal with excluded champs in a good way. Maybe hard code for ease
+    if champ_name not in exclude:
+        for skill in skills:
+            ability = None
+            try:
+                ability = skill.get("class")[1].split("_")[1]
+                if ability == "innate":
+                    ability = "p"
+            except IndexError:
+                print("Error with index", skill.get("class"))
 
-    for i in links:
-        urllib.request.urlretrieve(i, f"Ability_images/{i.split('/')[-3]}")
+            imgs = skill.find_all("img")
+            for i in imgs:
+
+                # Check that image not already used and isnt a small picture and that it is a champion image
+                if i.get("data-src") is not None and champ_name in i.get("data-src") and i.get("alt").split(".")[0] not in used_abilities \
+                        and "scale" not in i.get("data-src") and champ_name not in exclude and ability is not None:
+
+                    # Add links as keys to the dict with the ability 'tag' as value
+                    links[i.get("data-src")] = ability
+                    used_abilities.append(i.get("alt").split(".")[0])
+
+    for i in links.keys():
+        ability = links.get(i)
+        # TODO
+        # Use this code for later idk when (to get rid of numbers in dup abilities)
+        #if any(i.isdigit() for i in links):
+        #    ability = ability[:-2]
+        urllib.request.urlretrieve(i, f"Ability_images/{ability.upper()}_{i.split('/')[-3]}")
+
+
+def test():
+    t = open("exceptions.txt", "r")
+    for i in t.readlines():
+        print(f"\"{i.split()[0]}\"", end=", ")
+
+    t.close()
 
 
 def main():
